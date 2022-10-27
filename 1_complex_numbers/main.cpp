2479c0911d4d05faa2e17091f73faee8f019e1e0
ErikAramovich <erik.galstyan@student.rau.am>
2022-10-27 14:49:51 +0400

    Complex numbers

--- 1_complex_numbers/main.cpp
+++ 1_complex_numbers/main.cpp
@@ -1,11 +1,11 @@
-# include "complex.h"
+#include "complex.h"
 #include <vector>
 
 void sort(std::vector<float>& arr){
-	for(int i=1; i<arr.size(); ++i){
-		for(int j=i; j>0; --j){
-			if(arr[j]<arr[j-1]){
-				std::swap(arr[j-1],arr[j]);
+	for(int i = 1; i < arr.size(); ++i){
+		for(int j = i; j > 0; --j){
+			if(arr[j] < arr[j - 1]){
+				std::swap(arr[j - 1], arr[j]);
 			}
 			else{
 				break;
@@ -16,6 +16,7 @@ void sort(std::vector<float>& arr){
 
 int main()
 {
+	// creating Complex objects for checking overloaded operators
 	Complex num;
 	std::cout << "num looks: " << num << std::endl;
 	std::cout << "num.abs(): " << num.abs() << std::endl;
@@ -25,9 +26,9 @@ int main()
 
 	std::cout << "first looks : " << num_1 << std::endl;
 	std::cout << "second looks: " << num_2 << std::endl;
-	std::cout << "first+second: " << num_1 + num_2 << std::endl;
-	std::cout << "first-second: " << num_1 - num_2 << std::endl;
-	std::cout << "first*6     : " << num_1*6 << std::endl;
+	std::cout << "first + second: " << num_1 + num_2 << std::endl;
+	std::cout << "first - second: " << num_1 - num_2 << std::endl;
+	std::cout << "first * 6     : " << num_1 * 6 << std::endl;
 
 	Complex num_3(6, -1);
 	Complex num_4(3, 0);
@@ -38,19 +39,26 @@ int main()
 	Complex num_9(4, 3);
 	Complex num_10(2, 3);
 
+	// creating vector arr of Complex objects
 	std::vector<Complex> arr{num_1, num_2, num_3, num_4, num_5, num_6, num_7, num_8, num_9, num_10};
+
+	// creating float vector of Complex objects' absolute value
 	std::vector<float> sorted;
 	for(const auto& x: arr){
 		sorted.push_back(x.abs());
 	}
 
+	// printing float vector before sorting
 	std::cout << "Before sorting: ";
 	for(const auto& x: sorted){
 		std::cout << x << " ";
 	}
 	std::cout << std::endl;
 
+	// calling sort function for float vector
 	sort(sorted);
+
+	// printing sorted float vector
 	std::cout << "After sorting: ";
 	for(const auto& x: sorted){
 		std::cout << x << " ";
